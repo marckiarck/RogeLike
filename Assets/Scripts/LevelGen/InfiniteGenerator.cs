fc31b70f2b38cc05f9082a0eddbbcf169e715462
paula_cab <paula.cabezas@live.u-tad.com>
2022-06-22 21:34:43 +0200

    Enemies

--- Assets/Scripts/LevelGen/InfiniteGenerator.cs
+++ Assets/Scripts/LevelGen/InfiniteGenerator.cs
@@ -108,7 +108,7 @@ public class InfiniteGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount % 60 == 0)
+        if (Time.frameCount % 10 == 0)
         {
             CheckPlayerPos();
         }
