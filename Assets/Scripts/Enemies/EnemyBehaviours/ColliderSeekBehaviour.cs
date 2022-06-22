dc290873d5832c885d825b7bc22c7312c14dc6ff
marckiarck <marcos.romano@expersor.es>
2022-06-22 19:45:11 +0200

    Added enemy avoidance

--- Assets/Scripts/Enemies/EnemyBehaviours/ColliderSeekBehaviour.cs
+++ Assets/Scripts/Enemies/EnemyBehaviours/ColliderSeekBehaviour.cs
@@ -6,13 +6,18 @@ public class ColliderSeekBehaviour : SeekBehaviour
 {
     protected override void ExecuteBehaviour()
     {
+        if (ExecuteEnemyAvoidance())
+        {
+            return;
+        }
+
         Vector2 raycastDirection = objetive.transform.position - gameObject.transform.position;
 
         RaycastHit2D[] hits = Physics2D.RaycastAll(gameObject.transform.position, raycastDirection.normalized, raycastDirection.magnitude);
         bool collisionDetected = false;
         foreach (RaycastHit2D hit in hits)
         {
-            if (hit.collider != null && hit.collider.gameObject.tag == "Wall")
+            if (hit.collider != null && (hit.collider.gameObject.tag == "Wall"))
             {
                 collisionDetected = true;
                 break;
@@ -26,7 +31,7 @@ public class ColliderSeekBehaviour : SeekBehaviour
             collisionDetected = false;
             foreach (RaycastHit2D hit in verticalHits)
             {
-                if (hit.collider != null && hit.collider.gameObject.tag == "Wall")
+                if (hit.collider != null && (hit.collider.gameObject.tag == "Wall"))
                 {
                     collisionDetected = true;
                     break;
@@ -46,7 +51,7 @@ public class ColliderSeekBehaviour : SeekBehaviour
                 collisionDetected = false;
                 foreach (RaycastHit2D hit in horizontalHits)
                 {
-                    if (hit.collider != null && hit.collider.gameObject.tag == "Wall")
+                    if (hit.collider != null && (hit.collider.gameObject.tag == "Wall"))
                     {
                         collisionDetected = true;
                         break;
@@ -66,5 +71,45 @@ public class ColliderSeekBehaviour : SeekBehaviour
             base.ExecuteBehaviour();
         }
         
+    }
+
+    public bool ExecuteEnemyAvoidance()
+    {
+        Vector2 raycastDirection = objetive.transform.position - gameObject.transform.position;
+
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(gameObject.transform.position, 1f, raycastDirection.normalized, Time.deltaTime);
+
+        bool collisionDetected = false;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider != null && (hit.collider.gameObject.tag == "Enemy" && hit.collider.gameObject != gameObject))
+            {
+                Vector2 enemyDirection = hit.collider.gameObject.transform.position - gameObject.transform.position;
+                Vector2 objectiveDirection = objetive.transform.position - gameObject.transform.position;
+
+                if (enemyDirection.x * objectiveDirection.x > 0f)
+                {
+                    raycastDirection = new Vector2(0f, raycastDirection.y);
+                }
+
+                if (enemyDirection.y * objectiveDirection.y > 0f)
+                {
+                    raycastDirection = new Vector2(raycastDirection.x, 0f);
+                }
+
+
+                collisionDetected = true;
+                break;
+            }
+        }
+
+        if (collisionDetected)
+        {
+            moveDirection = raycastDirection;
+            Move();
+            return true;
+        }
+
+        return false;
     }
 }
